411cfca7b61b58f91e11cfb830aee90ca72262ce
John <sapanjo@sheridancollege.ca>
2020-01-28 14:49:16 -0500

    Initial Commit

---
+++ w4iPad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  w4iPad
+//
+//  Created by John on 2020-01-28.
+//  Copyright © 2020 John. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ w4iPadTests/w4iPadTests.swift
@@ -0,0 +1,34 @@
+//
+//  w4iPadTests.swift
+//  w4iPadTests
+//
+//  Created by John on 2020-01-28.
+//  Copyright © 2020 John. All rights reserved.
+//
+
+import XCTest
+@testable import w4iPad
+
+class w4iPadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
