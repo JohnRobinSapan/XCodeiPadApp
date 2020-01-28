119be9dff76ce24669f0c9dcbc840248c1d88e35
John <sapanjo@sheridancollege.ca>
2020-01-28 15:41:59 -0500

    Working state ViewController

--- w4iPad/ViewController.swift
+++ w4iPad/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, WKNavigationDelegate {
+    
+    @IBOutlet var wbPage : WKWebView!
+     @IBOutlet var activity : UIActivityIndicatorView!
+    @IBOutlet var txName : UITextField!
+    @IBOutlet var txEmail : UITextField!
+    
+    @IBOutlet var lbName : UILabel!
+    @IBOutlet var lbEmail : UILabel!
+    
+    
+    @IBAction func submit(sender : UIButton) {
+        lbName.text = txName.text
+        lbEmail.text = txEmail.text
+    }
 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let urlAddress = URL(string: "https://www.google.ca")
+        let url = URLRequest(url: urlAddress!)
+        
+        wbPage.load(url)
+        wbPage.navigationDelegate = self
+    }
+    
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        activity.isHidden = false
+        activity.startAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        activity.isHidden = true
+        activity.stopAnimating()
     }
 
 
